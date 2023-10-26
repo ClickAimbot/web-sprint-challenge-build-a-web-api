f8d777269ab3ef7068a7058414a29c71b6f2fc88
Austin <austin.amaral3@gmail.com>
2023-10-26 11:22:42 -0700

    wip

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,35 @@
-// Write your "actions" router here!
+const express = require('express');
+const Actions = require('./actions-model.js');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    Actions.get()
+        .then(actions => {
+            res.status(200).json(actions)
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Failed to get actions',
+            err: err.message,
+            stack: err.stack,
+            });
+        });
+});
+
+router.get('/:id', (req, res) => {
+    Actions.get(req.params.id)
+        .then(action => {
+            if (action) {
+                res.status(200).json(action)
+            } else {
+                res.status(404).json({ message: 'Action not found' })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Failed to get action',
+            err: err.message,
+            stack: err.stack,
+            });
+        });
+});
+
+module.exports = router;
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,48 @@
-// Write your "projects" router here!
+const express = require('express');
+const Projects = require('./projects-model.js');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    Projects.get()
+        .then(projects => {
+            res.status(200).json(projects)
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Failed to get projects',
+            err: err.message,
+            stack: err.stack,
+            });
+        });
+});
+
+router.get('/:id', (req, res) => {
+    Projects.get(req.params.id)
+        .then(project => {
+            if (project) {
+                res.status(200).json(project)
+            } else {
+                res.status(404).json({ message: 'Project not found' })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Failed to get project',
+            err: err.message,
+            stack: err.stack,
+            });
+        });
+});
+
+router.post('/', (req, res) => {
+    Projects.insert(req.body)
+        .then(project => {
+            res.status(201).json(project)
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Failed to create new project',
+            err: err.message,
+            stack: err.stack,
+            });
+        });
+});
+
+module.exports = router;
